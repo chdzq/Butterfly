131b1c0a6057c672dd90cd56c470eb5783f22237
zhangqi <zhangqi_248@qq.com>
2017-11-08 16:51:23 +0800

    增加AlamofireDownloader framework

---
+++ ButterflyDownloader/AlamofireDownloder/AlamofireDownloder.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireDownloder.h
+//  AlamofireDownloder
+//
+//  Created by iOS on 2017/11/8.
+//  Copyright © 2017年 chdzq.tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamofireDownloder.
+FOUNDATION_EXPORT double AlamofireDownloderVersionNumber;
+
+//! Project version string for AlamofireDownloder.
+FOUNDATION_EXPORT const unsigned char AlamofireDownloderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireDownloder/PublicHeader.h>
+
+
